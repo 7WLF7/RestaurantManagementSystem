494f0f313bde0a0f52c371baf5a898561cba3885
CosminCLG <fratilacosmin24@gmail.com>
2025-05-28 20:55:55 +0300

    update cart

--- RmsBackend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ RmsBackend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a292efe421ef7dd1a4776a4cb63ef64aa2de42d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1521c350cfeb1bb60ded1eaddc1928d8f8965570")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
